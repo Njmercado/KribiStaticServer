fdd6f00ec6ecb9f4b360d50060ba7b1c1933f071
Nino Jesus Mercado Consuegra <njmercado@uninorte.edu.co>
2019-06-01 19:05:34 -0500

    .....

---
+++ Public/precache-manifest.edf583ebbfa80c6f06ff3fb6f9ccb1c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "aec24ddac2654286ef28",
+    "url": "/js/chunk-57b87584.085165c3.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ff7fc3444d70bc0ce2fd1baaf8a49e0a",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "6dbb10e035510860de0f",
+    "url": "/js/chunk-vendors.5cf823f4.js"
+  },
+  {
+    "revision": "84ef7635bc577bcde36d",
+    "url": "/js/app.ca6647a7.js"
+  },
+  {
+    "revision": "dbcb9f73631d099b26a6",
+    "url": "/js/about.967ecc2d.js"
+  },
+  {
+    "revision": "d509d349abf512885a77bc763f5e760e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dbb10e035510860de0f",
+    "url": "/css/chunk-vendors.6ad0b682.css"
+  },
+  {
+    "revision": "aec24ddac2654286ef28",
+    "url": "/css/chunk-57b87584.58d25154.css"
+  },
+  {
+    "revision": "84ef7635bc577bcde36d",
+    "url": "/css/app.09e48c4f.css"
+  },
+  {
+    "revision": "dbcb9f73631d099b26a6",
+    "url": "/css/about.e880ec34.css"
+  }
+];
